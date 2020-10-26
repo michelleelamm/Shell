6b37daa53005d98eff2b6375a05dad4cc52e821e
Michelle Lam <michelleelamm@users.noreply.github.com>
2020-10-25 17:13:17 -0700

    Update command help

--- src/shell.c
+++ src/shell.c
@@ -1,14 +1,10 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+
 #include <unistd.h>
 #include <sys/wait.h>
-
-// To-do
-// Create makefile
-// Create log file of tests we can run and find cases where our shell fails
-// Create a instructions.txt
-// Create better printIntro() func
 
 
 void printDir() 
@@ -22,8 +18,24 @@ void printIntro(){
     printf("-----------------------------------------------------------------------\n");
     printf("\t\t\tTeam Swyft's Shell in C");
     printf("\n-----------------------------------------------------------------------\n");
+    printf("\nEnter \"help\" for list of commands.\n\n");
 
 }
+void printHelp() {
+    printf("\nWelcome to Team Swyft's help page!\n");
+    printf("\nBelow are the implemented commands for our shell: ");
+    printf("\ncd - change directory");
+    printf("\npwd - print working directory");
+    printf("\nmkdir - creates a new directory (does not create when directory already exists)");
+    printf("\nrmdir - removes a directory (does not remove if directory is nonempty)");
+    printf("\nls - lists contents");
+    printf("\ntouch - creates file");
+    printf("\ncp file1 file2 - copies contents from file1 to file2");
+    printf("\nexit - exit the shell");
+    printf("\nOur shell also can run executables. \n");
+    printf("\nThank you for using our Shell.\n\n");
+}
+
 int main(){
 
     printIntro();
@@ -34,10 +46,9 @@ int main(){
         char line[60]; //Arbitrary Length
         fgets(line, 60, stdin); //Read from console
         char *token; //Token will hold word
-
         char *commandArg[1024]; //Will store our tokenized strings
         int index = 0; //Will serve as our index and length for our tokenized string array
-
+        
         token = strtok(line, " \n");
         // Documentation for tokenizing: https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
         while( token != NULL){  
@@ -47,7 +58,6 @@ int main(){
         }
 
         commandArg[index] = NULL; //Must null-terminate our array or will continue reading remaining spaces
-
         // Determine if it was a built-in or system command and run accordingly
         char *builtInCmds[2] = {"cd","exit"};
         if(strcmp(commandArg[0], builtInCmds[0]) == 0){
@@ -57,11 +67,15 @@ int main(){
             printf("\n");
             exit(0);
         }
+        else if (strcmp(commandArg[0], "help") == 0) {
+            printHelp();
+            continue;
+        }
         else{
             // Create a child process to run command, parent process will wait until child termiantes
             if( fork() != 0) // Documentation: https://www.geeksforgeeks.org/fork-system-call/
                 wait(NULL); // Documentation: https://man7.org/linux/man-pages/man2/wait.2.html
-            else{
+            else{          
                 execvp(commandArg[0], commandArg);
                 exit(0); 
             }
